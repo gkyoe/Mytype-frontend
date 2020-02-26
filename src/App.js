79f67dfcedae61f7b81589fa28942f80b14f982f
김태선 <kirca322@gmail.com>
2020-02-26 13:07:21 +0900

    route initial setting2

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, Login, Main, Signup } from './pages';
 import Template from './components/Template';
 
-// function App() {
-// return (
-// 	<div>
-// 		<Switch>
-// 			<Route path='/home' render={() => <Home />} />
-// 			<Route path='/login' render={() => <Login />} />
-// 			<Route path='/Main' render={() => <Main />} />
-// 			<Route path='/Signup' render={() => <Signup />} />
-
-// 			<Route path='/' render={() => <Home />} />
-// 		</Switch>
-// 	</div>
-// );
-// }
-
 class App extends Component {
 	constructor(props) {
 		sueper(props);
@@ -32,16 +17,15 @@ class App extends Component {
 				<Switch>
 					<Route path="/home" render={() => <Home />} />
 					<Route path="/login" render={() => <Login />} />
-					<Route path="/Main" render={() => <Main />} />
-					<Route path="/Signup" render={() => <Signup />} />
-
+					<Route path="/main" render={() => <Main />} />
+					<Route path="/signup" render={() => <Signup />} />
 					<Route
 						path="/"
 						render={() => {
 							if (isLogin) {
-								return <Redirect to="/mypage" />;
+								return <Redirect to="/main" />;
 							}
-							return <Redirect to="/login" />;
+							return <Redirect to="/home" />;
 						}}
 					/>
 				</Switch>
