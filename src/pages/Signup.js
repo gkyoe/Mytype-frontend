0e0626a37640a13e9423ccef54d2267b55791e84
곽효은 <kyoeun@gwakyo-MacBookAir.local>
2020-03-02 02:50:45 +0900

    add function checking if passwords match & adding auto hypen

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,10 +7,83 @@ class Signup extends Component {
 		this.state = {
 			email: '',
 			password: '',
+			repassword: '',
 			username: '',
 			phonenumber: ''
 		};
+		this.handleInputValue = this.handleInputValue.bind(this);
+		this.addAutoHypen = this.addAutoHypen.bind(this);
+		this.handleInputValueAutoHypen = this.handleInputValueAutoHypen.bind(this);
+		this.doesPasswordMatch = this.doesPasswordMatch.bind(this);
+		this.renderFeedbackMessage = this.renderFeedbackMessage.bind(this);
+		this.handleInputValueAutoHypen = this.handleInputValueAutoHypen.bind(this);
 	}
+
+	// input에 들어오는 데이터를 state를 입력하는 함수
+	handleInputValue = key => e => {
+		this.setState({ [key]: e.target.value });
+	};
+
+	// 휴대폰 번호를 입력할 때 자동으로 하이픈('-')이 달리고, 입력 길이 제한이 되는 함수
+	addAutoHypen = str => {
+		str = str.replace(/[^0-9]/g, '');
+		var tmp = '';
+		var maxLength = 11;
+
+		if (str.length > maxLength) {
+			str = str.slice(0, maxLength);
+		}
+		if (str.length < 4) {
+			return str;
+		} else if (str.length < 7) {
+			tmp += str.substr(0, 3);
+			tmp += '-';
+			tmp += str.substr(3);
+			return tmp;
+		} else if (str.length < 11) {
+			tmp += str.substr(0, 3);
+			tmp += '-';
+			tmp += str.substr(3, 3);
+			tmp += '-';
+			tmp += str.substr(6);
+			return tmp;
+		} else {
+			tmp += str.substr(0, 3);
+			tmp += '-';
+			tmp += str.substr(3, 4);
+			tmp += '-';
+			tmp += str.substr(7);
+			return tmp;
+		}
+		return str;
+	};
+
+	// password 와 repassword 가 일치하는 확인하여 boolean 값 출력하는 함수
+	doesPasswordMatch() {
+		const { password, repassword } = this.state;
+		return password === repassword;
+	}
+
+	// 비밀번호가 일치하지 않으면 '패스워드가 일치하지 않습니다' 메세지를 출력하는 함수
+	renderFeedbackMessage() {
+		const { repassword } = this.state;
+
+		if (repassword) {
+			if (!this.doesPasswordMatch()) {
+				return (
+					<div className="invalid-feedback" style={{ fontSize: '11pt' }}>
+						비밀번호가 일치하지 않습니다
+					</div>
+				);
+			}
+		}
+	}
+
+	// 전화번호에 자동으로 하이픈을 넣어서 출력하고 state를 변경하는 함수
+	handleInputValueAutoHypen = key => e => {
+		e.target.value = this.addAutoHypen(e.target.value);
+		this.setState({ [key]: this.addAutoHypen(e.target.value) });
+	};
 
 	render() {
 		return (
@@ -26,6 +99,7 @@ class Signup extends Component {
 									type="email"
 									name="email"
 									placeholder="이메일을 입력 해주세요"
+									onChange={this.handleInputValue('email')}
 								/>
 							</td>
 						</tr>
@@ -37,6 +111,8 @@ class Signup extends Component {
 									type="password"
 									name="password"
 									placeholder="비밀번호를 입력 해주세요"
+									// onChange={this.handleInputValue('password')}
+									onChange={this.handleInputValue('password')}
 								/>
 							</td>
 						</tr>
@@ -50,6 +126,21 @@ class Signup extends Component {
 									type="password"
 									name="password"
 									placeholder="비밀번호를 다시 입력 해주세요"
+									onChange={this.handleInputValue('repassword')}
+								/>
+								{this.renderFeedbackMessage()}
+							</td>
+						</tr>
+						<tr>
+							<th className="signup-tb-left">이름</th>
+							<td className="signup-tb-right">
+								<input
+									className="signup-input-box"
+									type="text"
+									name="text"
+									placeholder="이름을 다시 입력 해주세요"
+									// onChange={this.handleInputValue('username')}
+									onChange={this.handleInputValue('username')}
 								/>
 							</td>
 						</tr>
@@ -62,6 +153,7 @@ class Signup extends Component {
 									name="tel"
 									placeholder="휴대폰 번호를 입력 해주세요"
 									pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+									onChange={this.handleInputValueAutoHypen('phonenumber')}
 								/>
 							</td>
 						</tr>
