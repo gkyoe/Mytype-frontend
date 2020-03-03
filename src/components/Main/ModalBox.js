ba74f073954f5bbdf77e719b42dc5fcb381bbdcc
곽효은 <kyoeun@gwakyo-MacBookAir.local>
2020-03-03 21:01:15 +0900

    유저 카테고리 생성버튼 클릭 시 기능구현

--- src/components/Main/ModalBox.js
+++ src/components/Main/ModalBox.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Router, Link } from 'react-router-dom';
+import * as LoginState from '../../modules/login';
+import * as addUserCategoryAction from '../../modules/addUserCategoryEntry';
+import axios from 'axios';
 
 class ModalBox extends Component {
 	constructor(props) {
@@ -7,29 +13,75 @@ class ModalBox extends Component {
 			inputValue: ''
 		};
 		this.handleInputValue = this.handleInputValue.bind(this);
+		this.closeModalAndSendInputVal = this.closeModalAndSendInputVal.bind(this);
 	}
 
 	handleInputValue = key => e => {
-		this.setState({ [key]: e.target.value });
+		console.log(e.target.value);
+		this.setState({ inputValue: e.target.value });
+		console.log(this.state);
+	};
+
+	closeModalAndSendInputVal = () => {
+		const { AddUserCategoryAction, closeModal } = this.props;
+		const sendInputValue = {
+			categoryName: this.state.inputValue
+		};
+
+		AddUserCategoryAction.addUserCategoryEntry(sendInputValue).then(
+			closeModal()
+		);
 	};
 
 	render() {
+		const { isLogin, categoryName } = this.props;
+		console.log(isLogin);
+
 		return (
-			<div className="modal">
-				<input
-					className="category-name-input"
-					placeholder="카테고리명을 입력해주세요"
-					onChange={this.handleInputValue('inputValue')}
-				></input>
-				<button className="modal-button" onClick={this.props.closeModal}>
-					추가
-				</button>
-				<button className="modal-button" onClick={this.props.closeModal}>
-					닫기
-				</button>
+			<div>
+				{isLogin ? (
+					<div className="modal">
+						<input
+							className="category-name-input"
+							placeholder="카테고리명을 입력해주세요"
+							onChange={this.handleInputValue('inputValue')}
+						></input>
+						<button
+							className="modal-button"
+							onClick={this.closeModalAndSendInputVal}
+						>
+							추가
+						</button>
+						<button
+							type="submit"
+							className="modal-button"
+							onClick={this.props.closeModal}
+						>
+							닫기
+						</button>
+					</div>
+				) : (
+					<div>
+						<div>로그인 하시겠습니까?</div>
+						<Link to="./Login">
+							<button>로그인</button>
+						</Link>
+						<button className="modal-button" onClick={this.props.closeModal}>
+							닫기
+						</button>
+					</div>
+				)}
 			</div>
 		);
 	}
 }
 
-export default ModalBox;
+export default connect(
+	state => ({
+		isLogin: state.login.isLogin,
+		categoryName: state.addUserCategoryEntry.categoryName
+	}),
+	dispatch => ({
+		AddUserCategoryAction: bindActionCreators(addUserCategoryAction, dispatch)
+	})
+)(ModalBox);
--- src/modules/addUserCategoryEntry.js
+++ src/modules/addUserCategoryEntry.js
@@ -1,1 +1,30 @@
+import { handleActions } from 'redux-actions';
+import axios from 'axios';
 
+const ADD_USER_CATEGORY = 'ADD_USER_CATEGORY';
+
+const initialState = {
+	categoryName: 'NULL'
+};
+function AddUserCategoryAPI(data) {
+	return axios.post(`http://localhost:3001/user/addCategory/`, data);
+}
+
+export const addUserCategoryEntry = data => dispatch => {
+	return AddUserCategoryAPI(data).then(result =>
+		dispatch({
+			type: ADD_USER_CATEGORY
+		})
+	);
+};
+
+export default handleActions(
+	{
+		[ADD_USER_CATEGORY]: (state, action) => {
+			return {
+				...state
+			};
+		}
+	},
+	initialState
+);
