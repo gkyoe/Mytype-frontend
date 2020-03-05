0c92306d6c96335765804c7893138b78ee555ed6
김태선 <kirca322@gmail.com>
2020-03-05 21:04:32 +0900

    최종디자인 직전

--- src/components/Home/HomeCategory.js
+++ src/components/Home/HomeCategory.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import HomeCategoryEntry from './HomeCategoryEntry';
 import './HomeCategory.css';
-import sad from './HomeCategoryImage/sad.jpg';
-import dance from './HomeCategoryImage/dance.jpg';
-import carmdown from './HomeCategoryImage/carmdown.png';
-import comfort from './HomeCategoryImage/comfort.jpeg';
+import corona from './HomeCategoryImage/corona.png';
+import vlog from './HomeCategoryImage/vlog.png';
+import eatting from './HomeCategoryImage/eatting.png';
+import travel from './HomeCategoryImage/travel.png';
+import netflix from './HomeCategoryImage/netflix.png';
 
 function HomeCategory() {
 	const data = [
 		{
 			id: 1,
 			title: '코로나',
-			image: sad
+			image: corona
 		},
 		{
 			id: 2,
 			title: '브이로그',
-			image: dance
+			image: vlog
 		},
 		{
 			id: 3,
 			title: '먹방',
-			image: carmdown
+			image: eatting
 		},
 		{
 			id: 4,
 			title: '여행',
-			image: comfort
+			image: travel
 		},
 		{
 			id: 5,
 			title: '넷플릭스 영화추천',
-			image: comfort
+			image: netflix
 		}
 	];
 
